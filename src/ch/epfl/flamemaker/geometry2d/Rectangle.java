cd00a59f7fe701271fed681d9b012f62560435b0
Loïc Serafin <loic.serafin@epfl.ch>
2013-02-19 14:32:24 +0100

    rectangle avec constructeur de base + javadoc

--- src/ch/epfl/flamemaker/geometry2d/Rectangle.java
+++ src/ch/epfl/flamemaker/geometry2d/Rectangle.java
@@ -1,5 +1,28 @@
 package ch.epfl.flamemaker.geometry2d;
 
 public class Rectangle {
-	//TODO Classe rectangle
+	Point center;
+	double width, height;
+	/**
+	 * Classe qui représente un rectangle géométrique.
+	 * @param center Le point central du rectangle
+	 * @param width La largeur du rectangle
+	 * @param height La hauteur du rectangle
+	 */
+	public Rectangle(Point center, double width, double height) {
+		this.center = center;
+		
+		if (width < 0){
+			throw new IllegalArgumentException("La largeur est négative.");
+		}
+		else{
+			this.width = width;
+		}
+		if (height < 0){
+			throw new IllegalArgumentException("La hauteur est négative.");
+		}
+		else{
+			this.height = height;
+		}
+	}
 }
