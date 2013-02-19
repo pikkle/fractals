db2425c9b0ea16d1fcd568bd404decd1384efd28
chrisgaubla <chrisgaubla@gmail.com>
2013-02-19 12:30:35 +0100

    bordel

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("flame makeeeeer");
+		System.out.println("flame makeeeeer bordel");
 	}
 
 }
