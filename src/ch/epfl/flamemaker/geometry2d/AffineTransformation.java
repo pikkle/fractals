b14fab2f926b0b80d0820b9b6c8a3b2cff06011a
Loïc Serafin <loic.serafin@epfl.ch>
2013-02-19 14:16:29 +0100

    implementation classes transformations

---
+++ src/ch/epfl/flamemaker/geometry2d/AffineTransformation.java
@@ -0,0 +1,11 @@
+package ch.epfl.flamemaker.geometry2d;
+
+public class AffineTransformation implements Transformation {
+
+	@Override
+	public Point transformPoint(Point p) {
+		//TODO retourne le point p transformé
+		return p;
+	}
+	
+}
