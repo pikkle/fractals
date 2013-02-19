be6b0e8d4f04f20ab0684d67b1c8348c03f0e3f5
Loïc Serafin <loic.serafin@epfl.ch>
2013-02-19 12:02:07 +0100

    hello christouffe prout

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Christouffe");
+		System.out.println("Hello Christouffe prout");
 	}
 
 }
