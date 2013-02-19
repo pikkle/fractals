d25e4a3859e824dbccaae4b2a16e485a72a7fcfd
Loïc Serafin <loic.serafin@epfl.ch>
2013-02-19 11:29:25 +0100

    c

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World huhu");
+		System.out.println("Hello World haha");
 	}
 
 }
