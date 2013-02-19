6967504f852964e51f972191dea308341c28cf14
chrisgaubla <chrisgaubla@gmail.com>
2013-02-19 14:34:37 +0100

    point

--- src/ch/epfl/flamemaker/geometry2d/Point.java
+++ src/ch/epfl/flamemaker/geometry2d/Point.java
@@ -1,5 +1,30 @@
 package ch.epfl.flamemaker.geometry2d;
 
 public class Point {
+	private double x;
+	private double y;
+	public static 
+	
+	public Point(double unX, double unY){
+		this.x = unX;
+		this.y = unY;
+	}
+	public double x(){
+		return x;
+	}
+	public double y()
+	{
+		return y;
+	}
+	public double r(){
+		return (Math.sqrt(x*x+y*y));
+	}
+	public double theta(){
+		return Math.atan2(x, y);
+		
+	}
+	public String toString(){
+		return("("+ x +", "+y+")");
+	}
 	//TODO
 }
