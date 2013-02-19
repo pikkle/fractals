c39880401229ae739e4f957f51a93c6c9c01b7fa
Loïc Serafin <loic.serafin@epfl.ch>
2013-02-19 21:17:53 +0100

    *Début classe

--- src/ch/epfl/flamemaker/geometry2d/AffineTransformation.java
+++ src/ch/epfl/flamemaker/geometry2d/AffineTransformation.java
@@ -1,7 +1,15 @@
 package ch.epfl.flamemaker.geometry2d;
 
 public class AffineTransformation implements Transformation {
-
+	private final double a,b,c,d,e,f;
+	AffineTransformation(double a, double b, double c, double d, double e, double f){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+		this.e = e;
+		this.f = f;
+	}
 	@Override
 	public Point transformPoint(Point p) {
 		//TODO retourne le point p transformé
