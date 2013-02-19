8e2db08382d3bc9bcd9eac7faf149cc45758845b
Loïc Serafin <loic.serafin@epfl.ch>
2013-02-19 14:04:28 +0100

    architecture

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,4 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("flame makeeeeer bordel le retour platinum 2");
 	}
-
 }
---
+++ src/ch/epfl/flamemaker/geometry2d/Rectangle.java
@@ -0,0 +1,5 @@
+package ch.epfl.flamemaker.geometry2d;
+
+public class Rectangle {
+	//TODO Classe rectangle
+}
