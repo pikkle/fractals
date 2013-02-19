3d1d158444683c9d4625718220e855f91c48018d
chrisgaubla <chrisgaubla@gmail.com>
2013-02-19 12:04:03 +0100

    fion

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Christouffe prout");
+		System.out.println("Hello Christouffe proutafion");
 	}
 
 }
