3b88828921050872e0e71a4f9027a150133b00b7
chrisgaubla <chrisgaubla@gmail.com>
2013-02-19 14:55:10 +0100

    point commenté

--- src/ch/epfl/flamemaker/geometry2d/Point.java
+++ src/ch/epfl/flamemaker/geometry2d/Point.java
@@ -1,5 +1,7 @@
 package ch.epfl.flamemaker.geometry2d;
-
+/**
+ * classe créant un point avec deux double (x et y)
+ */
 public class Point {
 	private final double X;
 	private final double Y;
@@ -10,20 +12,25 @@ public class Point {
 		this.X = unX;
 		this.Y = unY;
 	}
+	/**getter de X*/
 	public double x(){
 		return X;
 	}
+	/** getter de Y*/
 	public double y()
 	{
 		return Y;
 	}
+	/**Méthode qui retourne la coordonée r du point (son module)*/
 	public double r(){
 		return (Math.sqrt(X*X+Y*Y));
 	}
+	/** Méthode qui retourne l'angle théta du point */
 	public double theta(){
 		return Math.atan2(X, Y);
 		
 	}
+	/** Méthode qui retourne un String représentant le point par ses deux coordonées x et y ex: (1.5, 6.3)*/
 	public String toString(){
 		return("("+ X +", "+Y+")");
 	}
