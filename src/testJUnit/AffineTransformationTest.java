ee618e9aecf33aa606068bb61a90852e3e049833
chrisgaubla <chrisgaubla@gmail.com>
2013-02-22 12:59:29 +0100

    test aff trans

--- src/testJUnit/AffineTransformationTest.java
+++ src/testJUnit/AffineTransformationTest.java
@@ -5,11 +5,35 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ch.epfl.flamemaker.geometry2d.AffineTransformation;
+import ch.epfl.flamemaker.geometry2d.Point;
 
 public class AffineTransformationTest {
 	@Test
 	public void testNewTranslation(){
 		AffineTransformation at = AffineTransformation.newTranslation(1,1);
+		
+	}
+	@Test
+	public void testNewRotation(){
+		AffineTransformation at = AffineTransformation.newRotation(Math.PI/2);
+	}
+	@Test
+	public void testNewScaling(){
+		AffineTransformation at = AffineTransformation.newScaling(1, 1);
+	}
+	@Test
+	public void testNewShearX(){
+		AffineTransformation at = AffineTransformation.newShearX(1);
+	}
+	@Test
+	public void testNewShearY(){
+		AffineTransformation at = AffineTransformation.newShearY(1);
+	}
+	@Test
+	public void testTransformPoint(){
+		AffineTransformation at = AffineTransformation.newTranslation(2, 2);
+		Point p = new Point(1,1);
+		p = AffineTransformation.transformPoint(p);
 		
 	}
 
