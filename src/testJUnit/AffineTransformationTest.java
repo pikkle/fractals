a93ae36ec64bcaf488f2038d5367494d61aa95e2
chrisgaubla <chrisgaubla@gmail.com>
2013-02-22 12:28:40 +0100

    asd

---
+++ src/testJUnit/AffineTransformationTest.java
@@ -0,0 +1,16 @@
+package testJUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ch.epfl.flamemaker.geometry2d.AffineTransformation;
+
+public class AffineTransformationTest {
+	@Test
+	public void testNewTranslation(){
+		AffineTransformation at = newTranslation(1,1);
+		
+	}
+
+}
