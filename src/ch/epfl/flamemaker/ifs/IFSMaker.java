29567d9dc7a6a4d3fd7045ea59a910714a6a2b1f
Loïc Serafin <loic.serafin@epfl.ch>
2013-02-26 20:58:52 +0100

    *MEGA UBER FEUILLE

--- src/ch/epfl/flamemaker/ifs/IFSMaker.java
+++ src/ch/epfl/flamemaker/ifs/IFSMaker.java
@@ -11,6 +11,7 @@ import ch.epfl.flamemaker.geometry2d.Rectangle;
 
 public class IFSMaker {
 	public static void main(String[] args) throws FileNotFoundException {
+		
 		List<AffineTransformation> listTransfo = new ArrayList<AffineTransformation>();
 		listTransfo.add(new AffineTransformation(0, 0, 0, 0, 0.16, 0));
 		listTransfo.add(new AffineTransformation(0.2, -0.26, 0, 0.23, 0.22, 1.6));
@@ -19,12 +20,10 @@ public class IFSMaker {
 
 		IFS barnsley = new IFS(listTransfo);
 		IFSAccumulator ifsa = barnsley.compute(new Rectangle(new Point(0, 4.5),
-				6, 10), 120, 200, 150);
+				6, 10), 1200, 2000, 150);
 
 		PrintStream ps = new PrintStream("barnsley.PBM");
 
-		
-		
 		ps.println("P1");
 		ps.println(ifsa.width() + " " + ifsa.height());
 		for (int i = ifsa.height()-1; i >= 0; i--) {
