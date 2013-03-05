26a34565c087c10992dd4f5bb16c8ecb33ed1bfa
chrisgaubla <chrisgaubla@gmail.com>
2013-03-05 11:11:45 +0100

    flame haha

--- src/ch/epfl/flamemaker/flame/FlameAccumulator.java
+++ src/ch/epfl/flamemaker/flame/FlameAccumulator.java
@@ -1,18 +1,22 @@
 package ch.epfl.flamemaker.flame;
 
+import ch.epfl.flamemaker.geometry2d.AffineTransformation;
+import ch.epfl.flamemaker.geometry2d.Point;
+import ch.epfl.flamemaker.geometry2d.Rectangle;
+
 public final class FlameAccumulator {
 	private final int[][] hitCount;
 	private final double m;
 
-	private FlameAccumulator (int[][] hitCount) {
+	private FlameAccumulator(int[][] hitCount) {
 		this.hitCount = new int[hitCount.length][hitCount[0].length];
 		for (int i = 0; i < hitCount.length; i++) {
 			this.hitCount[i] = hitCount[i].clone();
 		}
-		int max =0;
-		for (int i =0 ; i < hitCount.length; i++){
-			for (int j =0; j < hitCount[i].length; j++){
-				if (hitCount[i][j]> max){
+		int max = 0;
+		for (int i = 0; i < hitCount.length; i++) {
+			for (int j = 0; j < hitCount[i].length; j++) {
+				if (hitCount[i][j] > max) {
 					max = hitCount[i][j];
 				}
 			}
@@ -30,11 +34,48 @@ public final class FlameAccumulator {
 	}
 
 	public double intensity(int x, int y) {
-		if (x < 0 | x > hitCount.length | y < 0 | y > hitCount[0].length) {
+		if (x < 0 || x > hitCount.length || y < 0 || y > hitCount[0].length) {
 			throw new IndexOutOfBoundsException(
 					"Les coordonées données ne sont pas valides");
 		}
 		return (Math.log(hitCount[x][y] + 1) / Math.log(m + 1));
+
+	}
+
+	public static class Builder {
+		private Rectangle frame;
+		private int[][] hitCount;
+		private AffineTransformation translationCadre, upscaleCadre, tuCadre;
+
+		public Builder(Rectangle frame, int width, int height) {
+			if (width <= 0 || height <= 0) {
+				throw new IllegalArgumentException(
+						"Les valeurs ne sont pas strictement positives");
+			}
+			this.frame = frame;
+			this.hitCount = new int[width][height];
+
+			translationCadre = AffineTransformation.newTranslation(
+					-(frame.left()), -(frame.bottom()));
+			upscaleCadre = AffineTransformation.newScaling(
+					width / frame.width(), height / frame.height());
+			tuCadre = upscaleCadre.composeWith(translationCadre);
+		}
+
+		public void hit(Point p) {
+			int px, py;
+			Point pT = tuCadre.transformPoint(p);
+			px = (int) pT.x();
+			py = (int) pT.y();
+			if (this.frame.contains(p)) {
+				this.hitCount[px][py]++;
+			}
+
+		}
+
+		public FlameAccumulator build() {
+			return new FlameAccumulator(this.hitCount);
+		}
 
 	}
 
