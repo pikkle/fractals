bf44846b5b927951e3f5fadb7c0405a3233535a8
Loïc Serafin <loic.serafin@epfl.ch>
2013-03-05 11:40:59 +0100

    *

--- src/ch/epfl/flamemaker/flame/FlameMaker.java
+++ src/ch/epfl/flamemaker/flame/FlameMaker.java
@@ -12,9 +12,9 @@ public class FlameMaker {
 	public static void main(String [] args){
 		List<FlameTransformation> listTransfo  = new ArrayList<FlameTransformation>();
 		//Shark fin
-		listTransfo.add(new FlameTransformation(new AffineTransformation(-0.4113504, -0.7124804, -0.4, 0.7124795, -0.4113508, 0.8), new double[]{1,0.1}));
-		listTransfo.add(new FlameTransformation(new AffineTransformation(-0.3957339, 0, -1.6, 0, -0.3957337, 0.2), new double[]{0.8,1}));
-		listTransfo.add(new FlameTransformation(new AffineTransformation(0.4810169, 0, 1, 0, 0.4810169, 0.9), new double[]{1}));
+		listTransfo.add(new FlameTransformation(new AffineTransformation(-0.4113504, -0.7124804, -0.4, 0.7124795, -0.4113508, 0.8), new double[]{1,0.1,0,0,0,0}));
+		listTransfo.add(new FlameTransformation(new AffineTransformation(-0.3957339, 0, -1.6, 0, -0.3957337, 0.2), new double[]{0,0,0,0,0.8,1}));
+		listTransfo.add(new FlameTransformation(new AffineTransformation(0.4810169, 0, 1, 0, 0.4810169, 0.9), new double[]{1,0,0,0,0,0}));
 		Flame flame = new Flame(listTransfo);
 		//Shark fin
 		FlameAccumulator flameaccu = flame.compute(new Rectangle(new Point(-0.25,0),5,4),500,400,50);
