1882ce857067de90dc82b5c39d490c46ffce45e9
chrisgaubla <chrisgaubla@gmail.com>
2013-03-05 12:14:56 +0100

    méthode compute bitch!

--- src/ch/epfl/flamemaker/flame/Flame.java
+++ src/ch/epfl/flamemaker/flame/Flame.java
@@ -2,6 +2,9 @@ package ch.epfl.flamemaker.flame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import ch.epfl.flamemaker.flame.Variation;
+import ch.epfl.flamemaker.flame.FlameTransformation;
 
 import ch.epfl.flamemaker.geometry2d.Point;
 import ch.epfl.flamemaker.geometry2d.Rectangle;
@@ -12,14 +15,29 @@ public class Flame {
 	public Flame(List<FlameTransformation> transformations) {
 		this.listTransfo = new ArrayList<FlameTransformation>(transformations);
 	}
-	
-	public FlameAccumulator compute(Rectangle frame, int width, int height, int density){
-		FlameAccumulator.Builder flameaccu = new FlameAccumulator.Builder(frame, width, height);
-		Point p = new Point (0,0);
-		for (int i =0; i< density*width*height; i++){
-			
+
+	public FlameAccumulator compute(Rectangle frame, int width, int height,
+			int density) {
+		FlameAccumulator.Builder flameaccu = new FlameAccumulator.Builder(
+				frame, width, height);
+		Point p = new Point(0, 0);
+		Random r = new Random();
+		for (int k = 0; k < density * width * height; k++) {
+			int i = r.nextInt(listTransfo.size());
+			double newX =0;
+			double newY =0;
+			for (int j = 0; j < Variation.ALL_VARIATIONS.size(); j++) {
+				double weight = this.listTransfo.get(i).getVariationWeight(j);
+				Point pTemp = Variation.ALL_VARIATIONS.get(j).transformPoint(
+						this.listTransfo.get(i).transformPoint(p));
+				newX += weight * pTemp.x();
+				newY += weight * pTemp.y();
+			}
+			p = new Point(newX,newY);	
+			flameaccu.hit(p);
 		}
-		
+		return flameaccu.build();
+
 	}
 
 }
--- src/ch/epfl/flamemaker/flame/FlameMaker.java
+++ src/ch/epfl/flamemaker/flame/FlameMaker.java
@@ -9,23 +9,30 @@ import ch.epfl.flamemaker.geometry2d.Point;
 import ch.epfl.flamemaker.geometry2d.Rectangle;
 
 public class FlameMaker {
-	public static void main(String [] args){
-		List<FlameTransformation> listTransfo  = new ArrayList<FlameTransformation>();
-		//Shark fin
-		listTransfo.add(new FlameTransformation(new AffineTransformation(-0.4113504, -0.7124804, -0.4, 0.7124795, -0.4113508, 0.8), new double[]{1,0.1,0,0,0,0}));
-		listTransfo.add(new FlameTransformation(new AffineTransformation(-0.3957339, 0, -1.6, 0, -0.3957337, 0.2), new double[]{0,0,0,0,0.8,1}));
-		listTransfo.add(new FlameTransformation(new AffineTransformation(0.4810169, 0, 1, 0, 0.4810169, 0.9), new double[]{1,0,0,0,0,0}));
+	public static void main(String[] args) {
+		List<FlameTransformation> listTransfo = new ArrayList<FlameTransformation>();
+		// Shark fin
+		listTransfo.add(new FlameTransformation(new AffineTransformation(
+				-0.4113504, -0.7124804, -0.4, 0.7124795, -0.4113508, 0.8),
+				new double[] { 1, 0.1, 0, 0, 0, 0 }));
+		listTransfo.add(new FlameTransformation(new AffineTransformation(
+				-0.3957339, 0, -1.6, 0, -0.3957337, 0.2), new double[] { 0, 0,
+				0, 0, 0.8, 1 }));
+		listTransfo.add(new FlameTransformation(new AffineTransformation(
+				0.4810169, 0, 1, 0, 0.4810169, 0.9), new double[] { 1, 0, 0, 0,
+				0, 0 }));
 		Flame flame = new Flame(listTransfo);
-		//Shark fin
-		FlameAccumulator flameaccu = flame.compute(new Rectangle(new Point(-0.25,0),5,4),500,400,50);
+		// Shark fin
+		FlameAccumulator flameaccu = flame.compute(new Rectangle(new Point(
+				-0.25, 0), 5, 4), 500, 400, 50);
 
 		PrintStream ps = new PrintStream("sharkfin.PGM");
 		ps.println("P2");
 		ps.println(flameaccu.width() + " " + flameaccu.height());
 		ps.println("100");
-		for (int i = flameaccu.height(); i>=0; i++) {
+		for (int i = flameaccu.height(); i >= 0; i++) {
 			for (int j = 0; j < flameaccu.width(); j++) {
-				ps.print(flameaccu.intensity(i,j));
+				ps.print(flameaccu.intensity(i, j));
 			}
 			ps.println();
 		}
