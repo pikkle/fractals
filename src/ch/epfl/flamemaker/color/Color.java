9d43a2902c110bef45d4af3519cfe9796abf9a09
Loic <Loic@TuxThink>
2013-04-04 14:14:58 +0200

    *javadoc de la classe Color *mozerfucking condition ternaire pour la méthode sRGBEncode
    *nettoyage de la classe principale FlamePPMMaker, ajout de la création
    de turbulence.PPM
    *création de la méthode statique printer, qui imprime le
    FlameAccumulator dans un fichier (avec couleurs)

--- src/ch/epfl/flamemaker/color/Color.java
+++ src/ch/epfl/flamemaker/color/Color.java
@@ -1,5 +1,9 @@
 package ch.epfl.flamemaker.color;
 
+/**
+ * Classe modélisant une couleur avec ses composantes Rouge, Verte et Bleue
+ *
+ */
 public final class Color {
 	double r,g,b;
 	final static public Color BLACK = new Color(0,0,0);
@@ -8,6 +12,14 @@ public final class Color {
 	final static public Color GREEN = new Color(0,1,0);
 	final static public Color BLUE = new Color(0,0,1);
 	
+	/**
+	 * Constructeur de couleur avec ses composantes RGB
+	 * (Les composantes sont comprises entre 0 et 1)
+	 * Lance l'exception IllegalArgumentException si les composantes ne sont pas valides.
+	 * @param r Composante rouge
+	 * @param g Composante verte
+	 * @param b Composante bleue
+	 */
 	public Color(double r, double g, double b){
 		if (r < 0 || r > 1 || g < 0 || g > 1 || b < 0 || b > 1){
 			throw new IllegalArgumentException("Les composantes de couleur données ne sont pas valides.");
@@ -16,20 +28,36 @@ public final class Color {
 		this.g = g;
 		this.b = b;
 	}
+	
 	public double red(){return this.r;}
 	public double green(){return this.g;}
 	public double blue(){return this.b;}
+	
+	/**
+	 * Mélange la couleur avec la couleur passée en paramètre.
+	 * La proportion s'applique sur la couleur appelée (et donc, le reste de la proportion sur la couleur en paramètre)
+	 * @param that La couleur qui se mélange avec la couleur appelée.
+	 * @param proportion La proportion de la couleur de base.
+	 * @return La couleur mélangée.
+	 */
 	public Color mixWith(Color that, double proportion){
 		if (proportion < 0 || proportion > 1){
 			throw new IllegalArgumentException("La proportion donnée n'est pas valide. = " + proportion + ")");
 		}
 		double r2, g2, b2;
+		//Additionne les composantes des deux couleurs avec les proportions adéquates.
 		r2 = proportion*this.r + (1-proportion)*that.r;
 		g2 = proportion*this.g + (1-proportion)*that.g;
 		b2 = proportion*this.b + (1-proportion)*that.b;
 		
 		return new Color(r2,g2,b2);
 	}
+	
+	/**
+	 * Retourne la couleur encodée dans un entier.
+	 * Chaque couleur occupe 8 bits et sont rangées dans l'ordre Rouge Vert Bleu.
+	 * @return L'entier représentant la couleur.
+	 */
 	public int asPackedRGB(){
 		int rInt = sRGBEncode(this.r, 255)<<16;
 		int gInt = sRGBEncode(this.g, 255)<<8;
@@ -38,13 +66,16 @@ public final class Color {
 		
 		
 	}
+	
+	/**
+	 * Calcule la valeur gamma encodée selon la norme sRGB.
+	 * @param v La valeur à encoder
+	 * @param max La valeur maximale de l'entier à retourner
+	 * @return La valeur encodée.
+	 */
 	public static int sRGBEncode(double v, int max){
-		if (v<= 0.0031308){
-			return (int) (max*(12.92*v));
-		}
-		else{
-			return (int) (max*((1.055*(Math.pow(v,(1/2.4)))-0.055)));
-		}
-		
+		// Condition ternaire qui retourne une valeur ou l'autre selon la valeur de v.
+		return (v <= 0.0031308) ? (int) (max*(12.92*v)) // si v est plus petit que 0.0031308
+				: (int) (max*((1.055*(Math.pow(v,(1/2.4)))-0.055))) ; //si v est plus grand que 0.0031308
 	}
 }
--- src/ch/epfl/flamemaker/flame/FlamePPMMaker.java
+++ src/ch/epfl/flamemaker/flame/FlamePPMMaker.java
@@ -8,17 +8,60 @@ import java.util.List;
 
 import ch.epfl.flamemaker.color.Color;
 import ch.epfl.flamemaker.color.InterpolatedPalette;
+import ch.epfl.flamemaker.color.Palette;
 import ch.epfl.flamemaker.geometry2d.AffineTransformation;
 import ch.epfl.flamemaker.geometry2d.Point;
 import ch.epfl.flamemaker.geometry2d.Rectangle;
 
 public class FlamePPMMaker {
+	/**
+	 * Méthode permettant d'écrire dans un fichier la fractale Flame.
+	 * @param flameAccu L'accumulateur de la Flame
+	 * @param palette La palette de couleur utilisée.
+	 * @param background La couleur de fond.
+	 * @param file Le fichier où l'image doit être stockée.
+	 */
+	private static void printer(FlameAccumulator flameAccu, Palette palette,
+			Color background, String file) {
+		System.out.println("Ecriture du fichier " + file);
+		try {
+			PrintStream ps = new PrintStream(file);
+			
+			//3 premières lignes qui indiquent que le fichier est une image .PPM
+			String start = "P3\n" + flameAccu.width() + " "
+					+ flameAccu.height() + "\n100";
+			ps.println(start);
+
+			for (int i = 0; i < flameAccu.height(); i++) {
+				for (int j = 0; j < flameAccu.width(); j++) {
+					//Ecriture ligne par ligne, colonne par colonne des pixels
+					Color colorPrint = flameAccu.color(palette, background, j,
+							i); //On redefinit pour chaque pixel la couleur du dit pixel.
+					ps.print(Color.sRGBEncode(colorPrint.red(), 100) + " "
+							+ Color.sRGBEncode(colorPrint.green(), 100) + " "
+							+ Color.sRGBEncode(colorPrint.blue(), 100) + " "); // Triplet R G B
+				}
+				ps.print("\n"); // Retour à la ligne pour la ligne de pixels suivante.
+			}
+			ps.flush();
+			ps.close(); // Fermeture du stream.
+		} catch (FileNotFoundException e) {
+			// Erreur du Stream: Le fichier n'existe pas.
+			System.out.println("Le fichier n'existe pas.");
+			e.printStackTrace();
+		}
+
+	}
+
 	public static void main(String[] args) throws FileNotFoundException {
+		
+		//Méthode principale qui crée les deux fractales Shark-fin et Turbulence
 		List<Color> listColor = Arrays.asList(new Color[] { Color.RED,
 				Color.GREEN, Color.BLUE });
 		InterpolatedPalette paletteInterpol = new InterpolatedPalette(listColor);
+		
 		// Shark fin
-		System.out.println("Création de la fractale Shark fin...");
+		System.out.println("Création de la fractale Shark-fin...");
 		List<FlameTransformation> listTransfoShark = new ArrayList<FlameTransformation>();
 		listTransfoShark.add(new FlameTransformation(new AffineTransformation(
 				-0.4113504, -0.7124804, -0.4, 0.7124795, -0.4113508, 0.8),
@@ -30,57 +73,28 @@ public class FlamePPMMaker {
 				0.4810169, 0, 1, 0, 0.4810169, 0.9), new double[] { 1, 0, 0, 0,
 				0, 0 }));
 		Flame flameShark = new Flame(listTransfoShark);
-		FlameAccumulator flameaccuShark = flameShark.compute(new Rectangle(
+		FlameAccumulator flameAccuShark = flameShark.compute(new Rectangle(
 				new Point(-0.25, 0), 5, 4), 500, 400, 50);
-
-		PrintStream ps1 = new PrintStream("sharkfin.PPM");
-		ps1.println("P3");
-		ps1.println(flameaccuShark.width() + " " + flameaccuShark.height());
-		ps1.println("100");
-		int h = flameaccuShark.height();
-		int w = flameaccuShark.width();
-		System.out.println("Création de l'image Shark fin...");
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				Color colorPrint = flameaccuShark.color(paletteInterpol,
-						Color.BLACK, j, i);
-				ps1.print(Color.sRGBEncode(colorPrint.red(), 100) + " "
-						+ Color.sRGBEncode(colorPrint.green(), 100) + " "
-						+ Color.sRGBEncode(colorPrint.blue(), 100) + " ");
-			}
-			ps1.print("\n");
-		}
-		ps1.flush();
-		ps1.close();
-
+		FlamePPMMaker.printer(flameAccuShark, paletteInterpol, Color.BLACK, "shark-fin.ppm");
+		
 		System.out.println();
-
-		/*
-		 * Turbulences
-		 * System.out.println("Création de la fractale Turbulences...");
-		 * List<FlameTransformation> listTransfoTurb = new
-		 * ArrayList<FlameTransformation>(); listTransfoTurb.add(new
-		 * FlameTransformation(new AffineTransformation( 0.7124807, -0.4113509,
-		 * -0.3, 0.4113513, 0.7124808, -0.7), new double[] { 0.5, 0, 0, 0.4, 0,
-		 * 0 })); listTransfoTurb.add(new FlameTransformation(new
-		 * AffineTransformation( 0.3731079, -0.6462417, 0.4, 0.6462414,
-		 * 0.3731076, 0.3), new double[] { 1, 0, 0.1, 0, 0, 0 }));
-		 * listTransfoTurb.add(new FlameTransformation(new AffineTransformation(
-		 * 0.0842641, -0.314478, -0.1, 0.314478, 0.0842641, 0.3), new double[] {
-		 * 1, 0, 0, 0, 0, 0 })); Flame flameTurb = new Flame(listTransfoTurb);
-		 * FlameAccumulator flameaccuTurb = flameTurb.compute(new Rectangle(new
-		 * Point( 0.1,0.1), 3,3), 500 , 500, 50);
-		 * 
-		 * PrintStream ps2 = new PrintStream("turbulences.PPM");
-		 * ps2.println("P3"); ps2.println(flameaccuTurb.width() + " " +
-		 * flameaccuTurb.height()); ps2.println("100"); int h2 =
-		 * flameaccuTurb.height(); int w2 = flameaccuTurb.width();
-		 * System.out.println("Création de l'image Turbulences..."); for (int i
-		 * = 0; i < h2; i++) { for (int j = 0; j < w2; j++) { ps2.print(((int)
-		 * (100*(flameaccuTurb.intensity(j, i)))) + " "); } ps2.print("\n"); }
-		 * ps2.flush(); ps2.close();
-		 */
-
+		
+		//Turbulence
+		System.out.println("Création de la fractale Turbulence...");
+		List<FlameTransformation> listTransfoTurb = new ArrayList<FlameTransformation>();				
+		listTransfoTurb.add(new FlameTransformation(new AffineTransformation(
+				0.7124807, -0.4113509, -0.3, 0.4113513, 0.7124808, -0.7),
+				new double[] { 0.5, 0, 0, 0.4, 0, 0 }));
+		listTransfoTurb.add(new FlameTransformation(new AffineTransformation(
+				0.3731079, -0.6462417, 0.4, 0.6462414, 0.3731076, 0.3), new double[] { 1, 0,
+				0.1, 0, 0, 0 }));
+		listTransfoTurb.add(new FlameTransformation(new AffineTransformation(
+				0.0842641, -0.314478, -0.1, 0.314478, 0.0842641, 0.3), new double[] { 1, 0, 0, 0,
+				0, 0 }));
+		Flame flameTurb = new Flame(listTransfoTurb);
+		FlameAccumulator flameAccuTurb = flameTurb.compute(new Rectangle(new Point(0.1,0.1),3,3), 500,500,50);
+		FlamePPMMaker.printer(flameAccuTurb, paletteInterpol, Color.BLACK, "turbulence.ppm");
+		
 		System.out.println("\nFin du programme.");
 	}
 }
